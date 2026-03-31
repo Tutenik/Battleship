ae1aba1e0d1e3dfaddc6362bb0bcb977b6fde9e4
Tutenik <duras.honza@gmail.com>
2026-03-31 21:25:57 +0200

    Added Ai strategy

---
+++ MVVM/AIStrategies/EasyAi.cs
@@ -0,0 +1,12 @@
+﻿using Battleship.MVVM.Model;
+
+namespace Battleship.MVVM.AIStrategies
+{
+    public class EasyAi : IAIStrategy
+    {
+        public bool MakeMove(GameBoard gameBoard)
+        {
+            return GameBoard.ShootCell(gameBoard.GetRandomCell());
+        }
+    }
+}
---
+++ MVVM/AIStrategies/IAIStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Battleship.MVVM.Model;
+
+namespace Battleship.MVVM.AIStrategies
+{
+    public interface IAIStrategy
+    {
+        bool MakeMove(GameBoard gameBoard);
+    }
+}
